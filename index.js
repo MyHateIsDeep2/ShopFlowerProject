a9606bbebd0941edf7e205c705bc9726c28fc7dd
MyHateIsDeep2 <winterxd53@gmail.com>
2026-01-13 19:13:46 +0100

    3. commit LOGIN FUNC

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(express.json())
 app.use(cors({origin: '*'}))
 
 const connection = {
@@ -24,42 +25,90 @@ app.get('/getBiljke', (request, response) => {
     return response.send(results);
     })
 });
+app.post('/registracija', (request, response) => {
+    const data = request.body;
+    const Korisnik = [data.Email, data.Lozinka];
 
-app.get('/getCart/:korisnik_id', (request, response) => {
-    const korisnik_id = req.params.korisnik_id;
-    conn.query("SELECT bBiljke.biljka_ime, bBiljke.cijena, bCart.kolicina, (bBiljke.cijena * bCart.kolicina) AS ukupno FROM bCart JOIN bBiljke ON bCart.produkt_id = bBiljke.biljka_id WHERE bCart.id_korisnik = ?", (error, results) => {
-        if (error) {
-            console.log(error)
-        }
-        return response.send(results);
-    })
-})
-
-app.post('/addtocart/:biljka_id/:korisnik_id', (req, res) => {
-    const id_biljke = req.params.biljka_id;
-    const id_korisnika = req.params.korisnik_id;
-    const kolicina = 1;
-
-    conn.query("INSERT INTO `bCart`(`korisnik_id`, `biljka_id`, `kolicina`) VALUES (?,?,1)", [id_korisnika, id_biljke],
+    conn.query(
+        "INSERT INTO bKorisnici (email, korisnik_password) VALUES (?, ?)", 
+        Korisnik, 
         (error, results) => {
-            if (error) console.log(error);
-            return res.send(results);
+            if (error) {
+                console.log(error);
+                return response.status(500).send({ error: "Database error" });
+            }
         }
     );
 });
 app.post('/login', (request, response) => {
-    const data = request.body;
-    console.log(data.Email);
-    console.log(data.Lozinka);
-    Korisnik = [data.Email, data.Lozinka]
-    conn.query("INSERT INTO bKorisnici (email, korisnik_password) VALUES (?, ?)", Korisnik, (error, results ) => {
+    const { Email, Lozinka } = request.body;
+
+    conn.query(
+        "SELECT korisnik_id FROM bKorisnici WHERE email = ? AND korisnik_password = ?",
+        [Email, Lozinka],
+        (error, results) => {
+            if (error) {
+                console.log(error);
+                return response.status(500).send({ error: "Database error" });
+            }
+
+            if (results.length === 0) {
+                return response.status(401).send({ error: "Neispravan email ili lozinka" });
+            }
+
+            return response.send({
+                korisnik_id: results[0].korisnik_id
+            });
+        }
+    );
+});
+
+app.get('/getCart/:korisnik_id', (request, response) => {
+    const korisnik_id = request.params.korisnik_id;
+    conn.query("SELECT bCart.produkt_id, bCart.id_korisnik, bCart.kolicina, bBiljke.biljka_ime, bBiljke.cijena, bBiljke.slika, bBiljke.opis, (bBiljke.cijena * bCart.kolicina) AS Ukupno FROM bCart JOIN bBiljke ON bCart.produkt_id = bBiljke.biljka_id WHERE bCart.id_korisnik = ?"), [korisnik_id], (error, results) => {
         if (error) {
             console.log(error)
-            return response.status(500).send(error);
         }
-        response.send({korisnik_id: results.insertId})
-    });
+        return response.send(results);
+    }
 });
+
+app.post('/addtocart/:korisnik_id/:biljka_id', (request, response) => {
+    const id_biljke = request.params.biljka_id;
+    const id_korisnika = request.params.korisnik_id;
+    const kolicina = 1;
+
+    conn.query("INSERT INTO `bCart`(`id_korisnik`, `produkt_id`, `kolicina`) VALUES (?,?,1)", [id_korisnika, id_biljke],
+        (error, results) => {
+            if (error) console.log(error);
+            return response.send(results);
+        }
+    );
+});
+app.post('/updateCart/:korisnik_id/:produkt_id', (request, response) => {
+    const { korisnik_id, produkt_id } = request.params;
+    const { kolicina } = request.body;
+    conn.query(
+        "UPDATE bCart SET kolicina = ? WHERE id_korisnik = ? AND produkt_id = ?",
+        [kolicina, korisnik_id, produkt_id],
+        (err, results) => {
+            if(err) console.log(err);
+            response.send(results);
+        }
+    );
+});
+app.delete('/removeCart/:korisnik_id/:produkt_id', (request, response) => {
+    const { korisnik_id, produkt_id } = request.params;
+    conn.query(
+        "DELETE FROM bCart WHERE id_korisnik = ? AND produkt_id = ?",
+        [korisnik_id, produkt_id],
+        (err, results) => {
+            if(err) console.log(err);
+            response.send(results);
+        }
+    );
+});
+
 app.get('/hello', (request, response) => {
     return response.send('Hello world');
 });
